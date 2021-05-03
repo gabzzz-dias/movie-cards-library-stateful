f1948a1a252e8ab54cc439556d6f1bdbcc20f527
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-03 16:46:12 -0300

    first commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
-// implement AddMovie component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="text-input1">
+          Inclui o texto:
+          <input type="text" id="text-input1" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
