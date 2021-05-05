1703d53bed88f410be895a47d833fb6ee19bae5e
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-04 21:40:49 -0300

    request 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>a</p>
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
