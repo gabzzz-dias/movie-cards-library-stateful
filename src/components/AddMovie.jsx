290c0b74a8db99bdf1f49cd49e1deb0458a592ad
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-04 21:23:47 -0300

    request 12

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,48 +4,25 @@ class AddMovie extends React.Component {
   render() {
     return (
       <form data-testid="add-movie-form">
-        <label
-          htmlFor="input-title"
-          data-testid="title-input-label"
-        >
+        <label htmlFor="input-title" data-testid="title-input-label">
           Título
-          <input
-            type="text"
-            id="input-title"
-            data-testid="title-input"
-          />
+          <input type="text" id="input-title" data-testid="title-input" />
         </label>
-        <label
-          htmlFor="input-subtitle"
-          data-testid="subtitle-input-label"
-        >
+        <label htmlFor="input-subtitle" data-testid="subtitle-input-label">
           Subtítulo
-          <input
-            type="text"
-            id="input-subtitle"
-            data-testid="subtitle-input"
-          />
+          <input type="text" id="input-subtitle" data-testid="subtitle-input" />
         </label>
-        <label
-          htmlFor="input-img"
-          data-testid="image-input-label"
-        >
+        <label htmlFor="input-img" data-testid="image-input-label">
           Imagem
-          <input
-            type="text"
-            id="input-img"
-            data-testid="image-input"
-          />
+          <input type="text" id="input-img" data-testid="image-input" />
         </label>
-        <label
-          htmlFor="textarea-input"
-          data-testid="storyline-input-label"
-        >
+        <label htmlFor="textarea-input" data-testid="storyline-input-label">
           Sinopse
-          <textarea
-            data-testid="storyline-input"
-            id="textarea-input"
-          />
+          <textarea data-testid="storyline-input" id="textarea-input" />
+        </label>
+        <label htmlFor="number-input" data-testid="rating-input-label">
+          Avaliação
+          <input type="number" id="number-input" data-testid="rating-input" />
         </label>
       </form>
     );
