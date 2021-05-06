c9b05779d25c77892a9e63cd3bb4272a50468e61
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-06 01:45:58 -0300

    request 13

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -20,16 +20,19 @@ class Genre extends React.Component {
         >
           <option
             data-testid="genre-option"
+            value="action"
           >
             Ação
           </option>
           <option
             data-testid="genre-option"
+            value="comedy"
           >
             Comédia
           </option>
           <option
             data-testid="genre-option"
+            value="thriller"
           >
             Suspense
           </option>
