052b5d5a629f50631cab90302fbe0757976f8239
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-06 02:17:33 -0300

    request 15-17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: props.movies,
+    };
+  }
+
   render() {
+    const { movies } = this.state;
     return (
       <main>
         <SearchBar />
-        <MovieList />
+        <MovieList movies={ movies } />
         <AddMovie />
       </main>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
 
 export default MovieLibrary;
